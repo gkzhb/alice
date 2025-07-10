9d0572bc35a7607ec9b5a181e3c3b4644bbc5ba4
gkzhb <gkzhb98@gmail.com>
2025-07-11 02:10:40 +0800

    feat: add storage to persist chat sessions

--- main.py
+++ main.py
@@ -3,6 +3,15 @@ from dotenv import load_dotenv
 from agno.agent import Agent
 from agno.models.litellm import LiteLLM
 from agno.tools import tool
+from agno.storage.sqlite import SqliteStorage
+
+# Create a storage backend using the Sqlite database
+storage = SqliteStorage(
+    # store sessions in the ai.sessions table
+    table_name="agent_sessions",
+    # db_file: Sqlite database file
+    db_file="data/dev/sessions.db",
+)
 
 load_dotenv()
 
@@ -19,6 +28,7 @@ def main():
         ),
         tools=[add_number],
         markdown=True,
+        storage=storage,
     )
     agent.print_response('计算 55+24', stream=True)
 
