6d8a38dd9ff242651f45d5d2b67cebb890fa3ee8
gkzhb <gkzhb98@gmail.com>
2025-07-12 02:26:49 +0800

    feat: use rich to prompt user to input

--- structured_output.py
+++ structured_output.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel, Field
 from agno.agent import Agent, RunResponse
 from alice.model.llm import ds_chat_model
+from rich.prompt import Prompt
 
 class TranslateScript(BaseModel):
     translatedText: str = Field(..., description="翻译后文本")
@@ -13,4 +14,7 @@ json_mode_agent = Agent(
     response_model=TranslateScript,
     use_json_mode=True,
 )
-json_mode_agent.print_response("New York")
+
+text = Prompt.ask("待翻译的英文文本")
+
+json_mode_agent.print_response(text, stream=True, stream_intermediate_steps=True, show_message=False)
